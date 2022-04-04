1d6eda6afea886e70da0b249125cda8f2aabd1f2
Kelvinskell <Kelvinskelll@gmail.com>
2022-04-04 20:06:49 +0100

    Update spelling.sh

--- scripts/spelling.sh
+++ scripts/spelling.sh
@@ -2,37 +2,57 @@
 # This script is currently beign worked on
 exec 2>/dev/null
 
-read file
+# Define dictionaries
+dict[0]=/usr/share/dict/american-english
+dict[1]=/usr/share/dict/british-english
+dict[2]=/usr/share/dict/words
+dict[3]=/usr/share/dict/cracklib-small
+changed="False"
+# Read file
+file=$1
 
 # Place file contents into an array
 file_array=( `cat "$file"` )
-echo ${file_array[@]}
-echo ${#file_array[*]}
 
-# Create an array of lowercase and uppercase  characters
-new_array1=()
-new_array2=()
-new_array3=()
+# Create an array of characters
+new_array=()
 for word in ${file_array[@]}
 do
-	new_array1+=(${word,,})
-	new_array2+=(${word^^})
-	new_array3+=(${word^})
+	new_array+=(${word})
 done
 
-# list to contain spelling errors
-spell=()
+# Array to contain spelling errors
+spelling_errors=()
 n=0
 
-for word in ${new_array1[@]}
+# Check spelling in dictionary
+for word in ${new_array[@]}
 do
-	if grep -qE "(^| )$word( |\$)" /usr/share/dict/american-english||grep -qE "(^| )$word( |\$)" /usr/share/dict/british-english||grep -qE "(^| )$word( |\$)" /usr/share/dict/words
+	if grep -qEi "(^| )$word( |\$)" ${dict[0]}||grep -qEi "(^| )$word( |\$)" ${dict[1]}||grep -qEi "(^| )$word( |\$)" ${dict[2]}||grep -qEi "(^| )$word( |\$)" ${dict[3]}
 	then 
 		
 		continue
 	else
-		spell[$n]="$word"
+		changed="True"
+		spelling_errors[$n]="$word"
 		((n++))
 	fi
 done
 
+# Display spelling errors if present
+if [[ $changed == "True" ]]
+then
+	if [[ ${#spelling_errors[@]} == 1 ]]
+	then
+		echo "The following word is incorrectly spelt: "
+	else
+		echo "The following words are incorrectly spelt: "
+	fi
+	for word in ${spelling_errors[@]}
+	do
+		echo "$word"
+	done
+else
+	echo "No spelling errors were found"
+fi
+exit 0
