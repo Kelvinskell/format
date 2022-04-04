9dacc5263aa80741981d186ec8396cc9964cfbd7
Kelvinskell <Kelvinskelll@gmail.com>
2022-04-04 20:59:14 +0100

    Update format.sh

--- format.sh
+++ format.sh
@@ -6,7 +6,7 @@ echo "Input filename"
 read file
 
 # Check if file exists
-if [ -f $file ]
+if [[ -f $file ]]
 then
 	# Create backup
 	cp $file $file.bak
@@ -16,7 +16,7 @@ else
 	exit 1 
 fi
 
-select opt in 'Convert comma seperated words into a list' 'Convert texts to upper or lower case'
+select opt in 'Convert comma seperated words into a list' 'Convert texts to upper or lower case' 'Check for spelling errors'
 do
 	if [[ $REPLY == 1 ]]
 	then
@@ -25,6 +25,10 @@ do
 	elif [[ $REPLY == 2 ]]
 	then
 		bash scripts/case.sh $file
+		exit 0
+	elif [[ $REPLY == 3 ]]
+	then
+		bash scripts/spelling.sh $file
 		exit 0
 	else
 		echo -e "You selected a wrong option. \nTry again."
