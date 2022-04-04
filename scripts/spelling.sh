756f5aa70800b40c4b372f49119959e0e2f0c6fc
Kelvinskell <Kelvinskelll@gmail.com>
2022-04-04 20:53:25 +0100

    Update RegEx for spellin errors

--- scripts/spelling.sh
+++ scripts/spelling.sh
@@ -2,7 +2,7 @@
 # This script is currently beign worked on
 exec 2>/dev/null
 
-# Define dictionaries
+# Define dictionary locations
 dict[0]=/usr/share/dict/american-english
 dict[1]=/usr/share/dict/british-english
 dict[2]=/usr/share/dict/words
@@ -14,19 +14,12 @@ file=$1
 # Place file contents into an array
 file_array=( `cat "$file"` )
 
-# Create an array of characters
-new_array=()
-for word in ${file_array[@]}
-do
-	new_array+=(${word})
-done
-
 # Array to contain spelling errors
 spelling_errors=()
 n=0
 
 # Check spelling in dictionary
-for word in ${new_array[@]}
+for word in ${file_array[@]}
 do
 	if grep -qEi "(^| )$word( |\$)" ${dict[0]}||grep -qEi "(^| )$word( |\$)" ${dict[1]}||grep -qEi "(^| )$word( |\$)" ${dict[2]}||grep -qEi "(^| )$word( |\$)" ${dict[3]}
 	then 
